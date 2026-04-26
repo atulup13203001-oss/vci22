077c8c4dea770d826e618dfebc54c8c1baa4210e
atulup13203001-oss <atulup13203001@gmail.com>
2026-04-26 15:47:18 -0400

    Update video_player_screen.dart

--- lib/screens/student/video_player_screen.dart
+++ lib/screens/student/video_player_screen.dart
@@ -1,100 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 class VideoPlayerScreen extends StatefulWidget {
   const VideoPlayerScreen({Key? key}) : super(key: key);
-
   @override
   State<VideoPlayerScreen> createState() => _VideoPlayerScreenState();
 }
 
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   late YoutubePlayerController _controller;
-  late String _videoUrl;
-  late String _title;
+  late String videoUrl;
+  late String title;
 
   @override
   void initState() {
     super.initState();
-    final arguments = Get.arguments as Map<String, dynamic>;
-    _videoUrl = arguments['url'] ?? '';
-    _title = arguments['title'] ?? 'Video Lecture';
-
-    // Extract video ID from YouTube URL
-    final videoId = YoutubePlayer.convertUrlToId(_videoUrl) ?? '';
-    
+    final args = Get.arguments as Map<String, dynamic>;
+    videoUrl = args['url'] as String;
+    title = args['title'] as String;
+    final videoId = YoutubePlayerController.convertUrlToId(videoUrl) ?? '';
     _controller = YoutubePlayerController(
-      initialVideoId: videoId,
-      flags: const YoutubePlayerFlags(
+      params: const YoutubePlayerParams(
+        showControls: true,
+        showFullscreenButton: true,
         autoPlay: true,
-        mute: false,
-        enableCaption: true,
       ),
     );
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _controller.loadVideoById(videoId: videoId);
+    });
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _controller.close();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(_title),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            YoutubePlayer(
-              controller: _controller,
-              showVideoProgressIndicator: true,
-              progressIndicatorColor: const Color(0xFFFF9500),
-              onReady: () {
-                // Player is ready
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    _title,
-                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  const SizedBox(height: 12),
-                  Container(
-                    padding: const EdgeInsets.all(12),
-                    decoration: BoxDecoration(
-                      color: Colors.blue.withOpacity(0.1),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Row(
-                      children: [
-                        const Icon(Icons.info, color: Colors.blue),
-                        const SizedBox(width: 8),
-                        Expanded(
-                          child: Text(
-                            'This video is playing within the app',
-                            style: Theme.of(context).textTheme.bodySmall,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+      appBar: AppBar(title: Text(title), centerTitle: true),
+      body: Column(
+        children: [
+          YoutubePlayerScaffold(
+            controller: _controller,
+            aspectRatio: 16 / 9,
+            builder: (context, player) => player,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text(title, style: Theme.of(context).textTheme.titleMedium),
+          ),
+        ],
       ),
     );
   }
